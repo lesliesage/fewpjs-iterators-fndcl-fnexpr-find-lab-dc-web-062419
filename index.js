04e01e00548d09aa1f4100eb63ea5215ecb01105
lesliesage <leslie.sage@gmail.com>
2019-08-05 15:33:01 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let win = record.find(function(year) {return year.result === "W"});
+  return win ? win.year : undefined;
+}
